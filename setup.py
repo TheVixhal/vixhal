4a1aad9ed7da8b8779e0613a14751900b5a73079
Vishal Singh Baraiya <142595758+TheVixhal@users.noreply.github.com>
2024-09-15 18:20:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'A library designed for secure key-value storage using AES encryption. It provides functionality for generating AES keys, encrypting and decrypting data, and converting between text and bytes. The library is designed for in-memory storage, making it suitable for fast, temporary data handling.'
 LONG_DESCRIPTION = open("README.md").read()
 
@@ -16,12 +16,13 @@ setup(
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
+    url="https://github.com/TheVixhal/vixhal",
     install_requires=['pycryptodome'],
     keywords=['python', 'vixhal', 'encrypt', 'decrypt', 'bytes', 'AES', 'store', 'retrieve', 'generate key', 'secure'],
     classifiers=[
-        "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ], 
     test_suit = "tests"
